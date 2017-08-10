397e23fa875260691bf09982e105fc80d473c602
John Clarke <themagoo@hotmail.co.uk>
2017-08-10 16:34:09 +0100

    German -> English
    Chnage the injected language

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var hello = require('./lib/say-hello');
 
-//hello.use(require('./lib/english-greeting'));
-hello.use(require('./lib/deutsche-greeting'));
+hello.use(require('./lib/english-greeting'));
+//hello.use(require('./lib/deutsche-greeting'));
 
 hello.getGreeting().then(
     (greeting) => {
