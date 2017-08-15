fa0191b6d3bd7cddabb4941ef54b1fea3e017367
UpbeatDragon <31000047+UpbeatDragon@users.noreply.github.com>
2017-08-15 08:02:13 +0100

    dutch!
    Unicorn!

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ hello.use(require('./lib/english-greeting'));
 //hello.use(require('./lib/polish-greeting'))
 //hello.use(require('./lib/spainish-greeting'))
 //hello.use(require('./lib/french-greeting'))
+hello.use(require('./lib/dutch-greeting'))
 hello.getGreeting().then(
     (greeting) => {
         console.log(greeting);
--- test/test-dutch-greeting.js
+++ test/test-dutch-greeting.js
@@ -6,6 +6,6 @@ describe('It supports dutch', () =>{
     it('Can say Hallo', () => {
         var greeter = require('../lib/dutch-greeting');
 
-        return greeter.getGreeting().should.eventually.equal('Hallo Welt!');
+        return greeter.getGreeting().should.eventually.equal('Hallo Wereld!');
     })
 })
